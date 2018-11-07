e0f1c7482ef5643a0c537b233d79bf7c4c4b80ac
Nirgiemand <henri.tamvere@khk.ee>
2018-11-07 15:12:29 +0200

    Challenge almost ready, comparison yet to do

--- OOP/OOP_1/src/com/HT/Ring.java
+++ OOP/OOP_1/src/com/HT/Ring.java
@@ -1,10 +1,45 @@
 package com.HT;
 
 public class Ring {
-    private int x;
-    private int y;
-    private String raadius;
-    private String varv;
+    double x;
+    double y;
+    double raadius;
+    String varv;
 
 
+    public Ring() {
+        this.x = this.y = this.raadius = 0;
+        this.varv = "värvitu";
+    }
+
+    public Ring(double raadius) {
+        this.x = this.y = 0;
+        this.raadius = raadius;
+        this.varv = "värvitu";
+    }
+
+    public Ring(double x, double y, double raadius, String varv) {
+        this.x = x;
+        this.y = y;
+        this.raadius = raadius;
+        this.varv = varv;
+    }
+
+    public double umbermoot () {
+        return Math.round(Math.PI * 2 * this.raadius);
+    }
+
+    public double pindala () {
+        return Math.round(Math.PI * Math.pow(this.raadius, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Ringi " +
+                "x=" + this.x +
+                ", y=" + this.y +
+                ", raadius=" + this.raadius +
+                ", varv='" + this.varv + '\'' +
+                '}';
+    }
 }
